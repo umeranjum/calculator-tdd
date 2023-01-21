e298ae7d1d861ed5aa519eb0e114f197a29d97bd
Umer Farooque <umaranjum.321@gmail.com>
2023-01-21 14:19:13 +0530

    Added number guessing game

--- src/main/java/com/example/demo/CalculatorApplication.java
+++ src/main/java/com/example/demo/CalculatorApplication.java
@@ -8,6 +8,8 @@ public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+//		NumberGuessingGame numberGuessingGame = new NumberGuessingGame();
+//		numberGuessingGame.start();
 	}
 
 }
---
+++ src/main/java/com/example/demo/NumberGuessingGame.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    private int answer;
+    private Scanner scanner;
+
+    //To start, check CalculatorApplication class. Remove commented code.
+
+    public NumberGuessingGame() {
+
+    }
+
+    public void start() {
+
+    }
+
+}
