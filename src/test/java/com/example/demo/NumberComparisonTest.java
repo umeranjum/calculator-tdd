d583229e0df259497c98fdb0d459426728fddb78
Umer Farooque <umaranjum.321@gmail.com>
2023-01-21 13:53:45 +0530

    Number comparison

---
+++ src/test/java/com/example/demo/NumberComparisonTest.java
@@ -0,0 +1,29 @@
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//public class NumberComparisonTest {
+//    private NumberComparison numberComparison = new NumberComparison();
+//
+//    @Test
+//    void testGreaterThan() {
+//        assertTrue(numberComparison.isGreaterThan(5, 3));
+//    }
+//
+//    @Test
+//    void testLessThan() {
+//        assertTrue(numberComparison.isLessThan(3, 5));
+//    }
+//
+//    @Test
+//    void testBetween() {
+//        assertTrue(numberComparison.isBetween(4, 3, 5));
+//    }
+//
+//    @Test
+//    void testEqual() {
+//        assertTrue(numberComparison.isEqual(5, 5));
+//    }
+//}
